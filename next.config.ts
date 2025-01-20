be4930d79c33e9823a042a87ad0a21b564f23a36
hailey-hy <lillyine.hy@gmail.com>
2025-01-21 00:13:22 +0900

    chore: 환경 변수 경로 세팅

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,43 @@
 import type { NextConfig } from "next";
+import dotenv from "dotenv";
+import path from "path";
+
+// 환경 변수 파일 경로 설정
+const envFilePath = path.resolve(
+    __dirname,
+    "pocket-prompt-frontend-v2-envs/.env.next." +
+        (process.env.NODE_ENV || "development")
+);
+
+// .env 파일 로드
+dotenv.config({ path: envFilePath });
 
 const nextConfig: NextConfig = {
     /* config options here */
     styledComponents: true,
+    reactStrictMode: true,
+    swcMinify: true,
+    env: {
+        NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+        NEXT_PUBLIC_WEB_URL: process.env.NEXT_PUBLIC_WEB_URL,
+        NEXT_PUBLIC_PORTONE_STORE_ID: process.env.NEXT_PUBLIC_PORTONE_STORE_ID,
+        NEXT_PUBLIC_PORTONE_CHANNEL_KEY:
+            process.env.NEXT_PUBLIC_PORTONE_CHANNEL_KEY,
+        NEXT_PUBLIC_FIREBASE_API_KEY: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+        NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN:
+            process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+        NEXT_PUBLIC_FIREBASE_PROJECT_ID:
+            process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+        NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET:
+            process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+        NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID:
+            process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+        NEXT_PUBLIC_FIREBASE_APP_ID: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+        NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID:
+            process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+        NEXT_PUBLIC_SENTRY_DSN: process.env.NEXT_PUBLIC_SENTRY_DSN,
+        SENTRY_AUTH_TOKEN: process.env.SENTRY_AUTH_TOKEN,
+    },
 };
 
 export default nextConfig;
