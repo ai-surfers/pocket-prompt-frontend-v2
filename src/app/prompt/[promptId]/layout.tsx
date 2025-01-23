4fabc8ef1bfab6eef05235e7135418885e834bbf
hailey-hy <lillyine.hy@gmail.com>
2025-01-23 14:46:01 +0900

    feat: 상세 프롬프트 동적 메타태그 적용

--- src/app/prompt/[promptId]/layout.tsx
+++ src/app/prompt/[promptId]/layout.tsx
@@ -1,28 +1,31 @@
+import { getPrompt } from "@/apis/prompt/prompt";
+import { defaultMetadata } from "@/app/layout";
 import { Metadata } from "next";
 
 interface Props {
-    params: { id: string }; // URL의 id 파라미터를 정의
+    params: { promptId: string }; // URL의 id 파라미터를 정의
 }
 
-// export async function generateMetadata({ params }: Props): Promise<Metadata> {
-//     const { id } = params;
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+    const { promptId } = params;
 
-//     const response = await fetch(`https://api.example.com/meta/${id}`, {
-//         next: { revalidate: 60 }, // 데이터를 60초마다 캐싱
-//     });
+    try {
+        // getPrompt 함수로 데이터 가져오기
+        const promptDetails = await getPrompt(promptId);
 
-//     if (!response.ok) {
-//         throw new Error("Failed to fetch metadata");
-//     }
+        // 가져온 데이터를 메타데이터로 반환
+        return {
+            title: promptDetails.title,
+            description: promptDetails.description,
+            keywords: promptDetails.title,
+        };
+    } catch (error) {
+        console.error("Error fetching prompt data for metadata:", error);
 
-//     const metaData = await response.json();
-
-//     return {
-//         title: metaData.title || "Default Title",
-//         description: metaData.description || "Default description",
-//         keywords: metaData.keywords || "default, keywords",
-//     };
-// }
+        // 실패 시 기본값 메타데이터 설정
+        return defaultMetadata;
+    }
+}
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
     return <main>{children}</main>;
