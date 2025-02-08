501f9f45ad0137f0b0e0cbc15741cab07212ff92
전해연 <93465128+hailey-hy@users.noreply.github.com>
2025-02-08 21:33:06 +0900

    [SEO] 프롬프트 상세 페이지 open graph 태그 추가 (#28)
    * feat: 프롬프트 상세 페이지 SEO 추가 설정
    
    * fix: seo url 추가
    
    * feat: 트위터용 태그 추가

--- src/app/prompt/[promptId]/layout.tsx
+++ src/app/prompt/[promptId]/layout.tsx
@@ -6,6 +6,8 @@ interface Props {
     params: { promptId: string }; // URL의 id 파라미터를 정의
 }
 
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
     const { promptId } = params;
 
@@ -19,8 +21,16 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
             description: promptDetails.description,
             keywords: promptDetails.title,
             openGraph: {
-                title: promptDetails.title,
+                ...defaultMetadata.openGraph,
+                title: `[프롬프트] ${promptDetails.title}`,
                 description: promptDetails.description,
+                url: `${baseUrl}/prompt/${promptDetails.id}`,
+            },
+            twitter: {
+                card: "summary_large_image",
+                title: `[프롬프트] ${promptDetails.title}`,
+                description: promptDetails.description,
+                images: [`${baseUrl}/img/images.png`],
             },
         };
     } catch (error) {
